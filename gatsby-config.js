ace64ee603f65d181cc8c36d8bb0a49abf6c9465
Henrique Jensen <henriquejensen@hotmail.com>
2021-06-12 18:50:57 -0300

    adds plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,10 @@
 module.exports = {
   siteMetadata: {
-    title: "My Blog",
+    title: "My First Gatsby Site",
   },
-  plugins: ["gatsby-plugin-gatsby-cloud"],
+  plugins: [
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 };
